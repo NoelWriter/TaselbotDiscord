5b9a7e112fe35456c4bdc62d9b4ed8b5d851a9e7
NoelWriter <schr0112@hz.nl>
2018-11-29 13:31:39 +0100

    Sellout fixed

--- cmd/sellout.js
+++ cmd/sellout.js
@@ -1,8 +1,8 @@
 module.exports.run = async (client, message, args) => {
-    message.channel.send(`Hosting costs money, every 16 cents is another day of >kech! https://paypal.me/pools/c/8a28x5GOp0`);
+    message.channel.send("Hosting costs money, every 16 cents is another day of >kech! https://paypal.me/pools/c/8a28x5GOp0");
   }
   module.exports.help = {
-    name: "Sellout",
+    name: "sellout",
     help: "Returns a paypall link",
     usage: ">sellout",
   }
